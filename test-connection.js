cc4b9914e05106e969cb2cc336ed2bafcdfa2c97
kmb_1972 <NicholasGossmeyer@gmail.com>
2026-03-17 14:36:03 -0500

    Update old files and add package.json

--- test-connection.js
+++ test-connection.js
@@ -1,13 +1,15 @@
-require('dotenv').config();
+require('dotenv').config({ path: ".env" });
 const { Pool } = require('pg');
 
-    const pool = new Pool({
-        host: process.env.DB_HOST,
-        database: process.env.DB_NAME,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        ssl: { rejectUnauthorized: false }
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER ,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
+    ssl: { rejectUnauthorized: false }
 });
+console.log('HOST:', process.env.DB_HOST);
 
 pool.query('SELECT NOW()', (err, res) => {
     if (err) {
