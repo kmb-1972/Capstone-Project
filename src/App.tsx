a2e42170166883d4a60669cec916e3c86860df41
kmb_1972 <NicholasGossmeyer@gmail.com>
2026-04-14 14:07:05 -0500

    Base React App

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+//import {useState} from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+
+function App() {
+  return (
+    <>
+      <div>Hello World</div>
+    </>
+  )
+}
+
+export default App
